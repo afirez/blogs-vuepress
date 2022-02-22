15c0082362327670e82debd09b9d4b29b3e649b0
afirez <afirez.io@gmail.com>
2022-02-22 11:21:21 +0800

    vuepress 2.x sidebar navbar

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,38 @@
+const { getMdByDir } = require("./utils");
 module.exports = {
-    title: "alphazz blog",
-    description: "This is a blog.",
-    base: '/blogs/' 
+    title: "alphazz",
+    // description: "This is a blog.",
+    base: '/blogs/',
+    // theme: '@vuepress/theme-default',
+    serviceWorker: true, // 阅读缓存进程
+    theme: '@vuepress/theme-default',
+    themeConfig: {
+        navbar: [
+          { text: 'Home', link: '/' },
+          { 
+              text: 'Tools',
+              children: ['/tools/markdown/markdown_grammar.md'],
+            //   children: [
+            //       {
+            //           text: "Markdown",
+            //         //   chilren: getMdByDir("tools/markdown", "tools/markdown")
+            //           chilren: ["/tools/markdown/markdown_grammar.md"]
+            //       }
+            //   ]
+          },
+          { text: 'About', link: '/about/' },
+        ],
+        sidebar: {
+          '/tools/': [
+              {
+                  text: "Markdown",
+                  collapsible: true,
+                //   chilren: getMdByDir("tools/markdown", "tools")
+                  chilren: ["/tools/markdown/markdown_grammar.md"]
+              }
+          ]
+        },
+        search: true,
+        searchMaxSuggestions: 10
+    }
 }
